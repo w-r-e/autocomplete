acd655e492d240ae9c7fc15e5631024e3cf9fb93
DaggBoot <148903817+DaggBoot@users.noreply.github.com>
2026-03-17 00:13:37 -0400

    fiNALLY implemented the damn dawg

--- dawg.py
+++ dawg.py
@@ -1,4 +1,3 @@
-from trie import Trie, TrieNode
 from tkinter import Tk, Entry, Listbox, END
 
 # TODO: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -7,30 +6,27 @@ from tkinter import Tk, Entry, Listbox, END
 # TODO: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
-class DAWGNode(TrieNode):
+class DAWGNode:
     """Class that represents a Node in the Directed Acyclic Word Graph (DAWG).
 
-    Inherited Attributes:
-        - children: A defaultdict containing the TrieNode children connected to this node.
-        - is_end: A boolean signifying wheter the node represents the end of a word.
-        - weight: The associated frequency of a word. Equals 0 unless is_end is true.
-        - max_weight: The maximum weight of all the children.
-
     Instance Attributes:
+        - children: A defaultdict containing the list of children connected to this node.
+        - is_end: A boolean signifying wheter the node represents the end of a word.
         - signature: The unique identifier for this node, used to for merging in the DAWG
                      minimization algorithm.
     """
-    __slots__ = ("signature",)
+    __slots__ = ("children", "is_end", "signature")
 
     def __init__(self):
-        super().__init__()  # Inherits the stuff from the TrieNode class
+        self.children = {}
+        self.is_end = False
         self.signature = None
 
     def gen_signature(self):
         """
         Creates a unique signature for this node: (is_end, weight, sorted children signatures)
         Used to detect identical subtrees for merging.
-        Assumes the children ahve the correct signature as this function will be used in a post order traversal.
+        Assumes the children have the correct signature as this function will be used in a post order traversal.
         """
 
         # Generates the signature... Just used so that we can compare nodes and then say "HEY! these two are the same"
@@ -41,180 +37,139 @@ class DAWGNode(TrieNode):
         # would make it so that merging would combine the weights, which means that they would be considered equually
         # likely event if they aren't.
 
-        if self.is_end:
-            # Word-end nodes are never merged
-            self.signature = (self.is_end,)
-        else:
-            # Non-end nodes can be merged if children are identical
-            children_sig = tuple(sorted((char, child.signature)) for char, child in self.children.items())
-            self.signature = (self.is_end, children_sig)
+        child_items = []
+        for ch, child in sorted(self.children.items()):
+            # child.signature must already be computed
+            # Works since we work post-order in the DAWG
+            child_items.append((ch, child.signature))
+        self.signature = (self.is_end, tuple(child_items))
+        return self.signature
 
 
-class DAWG:
+class IncrementalDAWG:
     """
-    Class that represents a Directed Acylic Word Graph (DAWG).
+    Incremental construction of a Directed Acyclic Word Graph (DAWG).
+
+    This class allows words to be inserted one at a time in !!!!lexicographical order!!!!,
+    maintaining a minimal DAWG at all times. It is based on the algorithm by Daciuk et al. (2000).
 
     Instance Attributes:
-        - root: The root node, all its children represent the starting letters of words.
-        - register: A mapping between a DAWGNode and its corresponding signature.
+        - root: The root node of the DAWG. All words are reachable from this node.
+        - prev_word: The previously inserted word. Used to compute the common prefix
+          between consecutive insertions.
+        - unchecked: A stack of nodes representing the "unprocessed" part of the
+          last inserted word. Each element is a tuple of (parent_node, character, child_node).
+          These nodes may be merged during minimization.
+        - register: A mapping from node signatures to DAWGNode instances. Used to detect
+          identical subtrees for merging in order to maintain minimality.
     """
     root: DAWGNode
-    register: dict[tuple, DAWGNode]
-    # TODO: You can see in my implementation, I fail to update max_weights, because now that we merge nodes, there are new children
+    prev_word: str
+    unchecked: list[tuple[DAWGNode, str, DAWGNode]]
+    register: dict
 
-    def __init__(self, trie: Trie):
-        self.root = self._copy_node(trie.root)  # Copies the entire Trie Down First
+    def __init__(self):
+        self.root = DAWGNode()
+        self.prev_word = ""
+        self.unchecked = []
         self.register = {}
-        self.root = self._minimize(self.root)  # Then minimizes, i.e. combined nodes that are the same.
 
-    def _copy_node(self, node: TrieNode) -> DAWGNode:
-        """Recursively copy a TrieNode into a DAWGNode"""
-        new_node = DAWGNode()  # Set up a node
-        new_node.is_end = node.is_end
-        new_node.weight = node.weight
-        new_node.max_weight = node.max_weight  # These 3 lines just copy the properties of the node
-        for char, child in node.children.items():
-            new_node.children[char] = self._copy_node(child)  # Makes you copy all the children and their children...
-        return new_node
+    def insert(self, word):
+        """Inserts the characters of a word into the DAWG."""
+        if word < self.prev_word:
+            raise ValueError("Words must be inserted in lexicographic order")
 
-    def _minimize(self, node: DAWGNode):
-        """Recursively minimize the node's subtrees into the registry by traversing post-order"""
-        for char, child in list(node.children.items()):
-            node.children[char] = self._minimize(child)  # This traverses to the bottom of the Trie and begins working
+        # First this bit finds the common prefix yk like if the most recent word was "carp" and the new one is "cat"
+        # it compares the characters to see which ones are common, and stops when it finds a difference
+        # Here for carp and cat it'll compare until it hits the 3rd character, see t =/= r and, then we have "ca"
+        # which is the correct common prefix. Techincally we only store its position,
+        common = 0
+        for a, b in zip(word, self.prev_word):
+            if a != b:
+                break
+            common += 1
 
-        node.gen_signature()  # Generates the signature of a node. We do this here because the signature is only right
-        # if we make sure all the children signatures are right, that why its here and not in the DAWGNode class.
+        # Calls this to minimize the new nodes in the suffix, i.e. compare them with exisiting nodes and merge
+        self._minimize_suffix(common)
 
-        if not node.is_end and node.signature in self.register:  # If we've already seen this node before
-            return self.register[node.signature]  # Use the old exisitng node and ignore the current one
-        else:
-            self.register[node.signature] = node  # Otherwise it's a new node so we save it to the register and continue
-            return node
-
-    def search(self, prefix: str, k: int = 10) -> list[str]:
-        """Search the minimized DAWG (top-k suggestions)"""
-        # This one works the same as the Trie
         node = self.root
-        for char in prefix:
-            if char not in node.children:
-                return []
+        for char in word[:common]:
             node = node.children[char]
 
-        suggestions = []
-        self._find_suggestions(node, prefix, suggestions, k, -1)
-        return [word for word, _ in suggestions]
+        for char in word[common:]:
+            new = DAWGNode()
+            node.children[char] = new
+            self.unchecked.append((node, char, new))
+            node = new
 
-    def _find_suggestions(self, node: DAWGNode, prefix: str, suggestions: list, k: int, min_weight: int):
-        # This one works the same as the Trie
-        """Recursively traverse the DAWG to get top-k suggestions"""
+        node.is_end = True
+        self.prev_word = word
+
+    def _minimize_suffix(self, down_to):
+        """Recursivly minimises the suffix of a word"""
+        for i in range(len(self.unchecked) - 1, down_to - 1, -1):
+            parent, char, child = self.unchecked[i]
+
+            sig = child.gen_signature()
+
+            if sig in self.register:
+                parent.children[char] = self.register[sig]
+            else:
+                self.register[sig] = child
+
+            self.unchecked.pop()
+
+    def search(self, prefix: str) -> list[str]:
+        """Return all words starting with prefix."""
+        node = self.root
+        for ch in prefix:
+            if ch not in node.children:
+                return []
+            node = node.children[ch]
+        # Now collect all completions
+        results = []
+        self._collect(node, prefix, results)
+        return results
+
+    def _collect(self, node: DAWGNode, path: str, results: list[str]) -> None:
+        """Recursively collects all words that have the prefix 'path'"""
         if node.is_end:
-            suggestions.append((prefix, node.weight))
-            suggestions.sort(key=lambda x: x[1], reverse=True)
-            if len(suggestions) > k:
-                suggestions.pop()
-            if len(suggestions) == k:
-                min_weight = suggestions[-1][1]
-
-        for char, child in node.children.items():
-            if child.max_weight <= min_weight:
-                continue
-            self._find_suggestions(child, prefix + char, suggestions, k, min_weight)
-
-    # registry contians the unqiue subtrees already processed, signature: node where signature is the unique marker
-    # need to minimise:
-    # merge two identical subtrees ->:
-    # go to all the children, compute signature, check if it exists in the registry, merge,
-    # update max weight so post order processing
-
-# TODO: PLEASE IGNORE THIS ITS A WORK IN FUCKING PROGRESS
-# class IncrementalDAWG:
-#     root: DAWGNode
-#     prev_word: str
-#     unchecked: list
-#     register: dict
-#
-#     def __init__(self):
-#         self.root = DAWGNode()
-#         self.prev_word = ""
-#         self.unchecked = []
-#         self.register = {}
-#
-#     def insert(self, word, weight):
-#         if word < self.prev_word:
-#             raise ValueError("Words must be inserted in lexicographic order")
-#
-#         # First this bit finds the common prefix yk like if the most recent word was "carp" and the new one is "cat"
-#         # it compares the characters to see which ones are common, and stops when it finds a difference
-#         # Here for carp and cat it'll compare until it hits the 3rd cahracter, see t =/= r and, then we have "ca"
-#         # which is the correct common prefix. Techincally we only store its position,
-#         common = 0
-#         for a, b in zip(word, self.prev_word):
-#             if a != b:
-#                 break
-#             common += 1
-#
-#         self._minimize(common)
-#
-#         node = self.root
-#         for char in word[:common]:
-#             node = node.children[char]
-#
-#         for char in word[common:]:
-#             new = DAWGNode()
-#             node.children[char] = new
-#             self.unchecked.append((node, char, new))
-#             node = new
-#
-#         node.is_end = True
-#         node.weight = weight
-#         self.previous_word = word
-#
-#     def _minimize(self, down_to):
-#         for i in range(len(self.unchecked) - 1, down_to - 1, -1):
-#             parent, char, child = self.unchecked[i]
-#             sig = child.signature()
-#
-#             if sig in self.register:
-#                 parent.children[char] = self.register[sig]
-#             else:
-#                 self.register[sig] = child
-#
-#             self.unchecked.pop()
+            results.append(path)
+        for ch, child in sorted(node.children.items()):
+            self._collect(child, path + ch, results)
 
 
 def update_suggestions(dawg, entry, suggestions_listbox):
     """Updates the suggestions based on the changes from the recent user entry."""
     prefix = entry.get()
-    if not prefix:
-        suggestions_listbox.delete(0, END)
-        return
-    suggestions = dawg.search(prefix, k=10)
     suggestions_listbox.delete(0, END)
-    for suggestion in suggestions:
-        suggestions_listbox.insert(END, suggestion)
+    if not prefix:
+        return
+    suggestions = dawg.search(prefix)[:10]  # top 10
+    for word in suggestions:
+        suggestions_listbox.insert(END, word)
 
 
 def load_words(filename):
     """Method used to load words from a local CSV."""
-    words = dict()
+    words = []
     with open(filename, 'r') as f:
         for line in f:
-            word, count = line.strip().split(',', 1)
-            words[word] = int(count)
-    return words
+            word, _ = line.strip().split(',', 1)
+            words.append(word)
+    return sorted(words)
 
 
 def main():
     """Main function that packages the Trie setup"""
     # Set the path to the file you'd like to load
     file_path = "unigram_freq.csv"
-    trie = Trie()
+    dawg = IncrementalDAWG()
 
     words = load_words(file_path)
-    for word, count in words.items():
-        trie.insert(word, count)
+    for word in words:
+        dawg.insert(word)
 
-    dawg = DAWG(trie)
     root = Tk()
     root.config(bg="dark grey")
     root.title("Autocomplete App")
@@ -225,9 +180,110 @@ def main():
     suggestions_listbox = Listbox(root, width=50, bg="black", fg="light blue", selectbackground="blue")
     suggestions_listbox.pack(padx=10, pady=10)
 
-    entry.bind("<KeyRelease>", lambda event: update_suggestions(dawg, entry, suggestions_listbox))
+    entry.bind("<KeyRelease>", lambda _: update_suggestions(dawg, entry, suggestions_listbox))
     root.mainloop()
 
 
 if __name__ == "__main__":
     main()
+
+
+# THE MESS
+# class DAWG:
+#     """
+#     Class that represents a Directed Acylic Word Graph (DAWG).
+#
+#     Instance Attributes:
+#         - root: The root node, all its children represent the starting letters of words.
+#         - register: A mapping between a DAWGNode and its corresponding signature.
+#     """
+#     root: DAWGNode
+#     register: dict[tuple, DAWGNode]
+#
+#     def __init__(self, trie: Trie):
+#         self.root = self._copy_node(trie.root)  # Copies the entire Trie Down First
+#         self.register = {}
+#         self.root = self._minimize(self.root)  # Then minimizes, i.e. combined nodes that are the same.
+#
+#     def _copy_node(self, node: TrieNode) -> DAWGNode:
+#         """Recursively copy a TrieNode into a DAWGNode"""
+#         new_node = DAWGNode()  # Set up a node
+#         new_node.is_end = node.is_end
+#         new_node.weight = node.weight
+#         new_node.max_weight = node.max_weight  # These 3 lines just copy the properties of the node
+#
+#         for char, child in node.children.items():
+#             new_node.children[char] = {self._copy_node(child)}
+# Makes you copy all the children and their children...
+#
+#         return new_node
+#
+#     def _minimize(self, node: DAWGNode):
+#         """Recursively minimize the node's subtrees into the registry by traversing post-order"""
+#         # This traverses to the bottom of the Trie and begins working
+#         for char, nodes in list(node.children.items()):
+#             minimized_nodes = set()
+#             for child in nodes:
+#                 minimized = self._minimize(child)
+#                 minimized_nodes.add(minimized)  # Set handles duplicates automatically
+#             node.children[char] = minimized_nodes
+#
+#         node.gen_signature()  # Generates the signature of a node. We do this here because the signature is only right
+#         # if we make sure all the children signatures are right, that why its here and not in the DAWGNode class.
+#
+#         if not node.is_end and node.signature in self.register:  # If we've already seen this node before
+#             return self.register[node.signature]  # Use the old exisitng node and ignore the current one
+#         else:
+#             self.register[node.signature] = node
+# Otherwise it's a new node so we save it to the register and continue
+#             return node
+#
+#     def search(self, prefix: str, k: int = 10) -> list[str]:
+#         """Search the minimized DAWG (top-k suggestions)"""
+#         # Start with a set containing the root
+#         current_nodes = {self.root}
+#
+#         for char in prefix:
+#             next_nodes = set()
+#             for node in current_nodes:
+#                 if char in node.children:
+#                     # Add ALL possible nodes for this character
+#                     next_nodes.update(node.children[char])
+#
+#             if not next_nodes:  # No valid paths
+#                 return []
+#             current_nodes = next_nodes
+#
+#         # Now collect suggestions from all possible paths
+#         suggestions = []
+#
+#         for node in current_nodes:
+#             self._find_suggestions(node, prefix, suggestions, k, -1, set())
+#
+#         # sort final suggestions
+#         suggestions.sort(key=lambda x: x[1], reverse=True)
+#         return [word for word, _ in suggestions]
+#
+#     def _find_suggestions(self, node: DAWGNode, prefix: str, suggestions: list,
+#                           k: int, min_weight: int, seen_words: set):
+#         """Recursively traverse the DAWG to get top-k suggestions"""
+#         if node.is_end and prefix not in seen_words:
+#             seen_words.add(prefix)
+#             suggestions.append((prefix, node.weight))
+#             suggestions.sort(key=lambda x: x[1], reverse=True)
+#             if len(suggestions) > k:
+#                 suggestions.pop()
+#             if len(suggestions) == k:
+#                 min_weight = suggestions[-1][1]
+#
+#         for char, child_nodes in node.children.items():
+#             for child in child_nodes:
+#                 if child.max_weight <= min_weight:
+#                     continue
+#                 self._find_suggestions(child, prefix + char, suggestions, k, min_weight, seen_words)
+#
+#     # registry contians the unqiue subtrees already processed, signature: node where signature is the unique marker
+#     # need to minimise:
+#     # merge two identical subtrees ->:
+#     # go to all the children, compute signature, check if it exists in the registry, merge,
+#     # update max weight so post order processing
